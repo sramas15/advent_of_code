56fd044eacdef26033a167623b51b6d4357a5da3
Sheila Ramaswamy <sheilar@alumni.stanford.edu>
2021-11-30 22:02:49 -0800

    use lambdas

--- 2021/day1.py
+++ 2021/day1.py
@@ -1,36 +1,30 @@
 import sys
 
-def preprocess(file_path):
+def parse(file_path):
     with open(file_path) as f:
         return [int(l.strip()) for l in f]
 
-def part1(processed):
+def get_num_increases(data, lam):
     num_increases = 0
-    m = len(processed)
-    last = processed[0]
+    m = len(data)
+    last = lam(0, m, data)
     for i in range(1, m):
-        cur = processed[i]
+        cur = lam(i, m, data)
         if cur > last:
             num_increases += 1
         last = cur
     return num_increases
 
+def part1(data):
+    p1 = lambda i, m, data: data[i]
+    return get_num_increases(data, p1)
 
-def part2(processed):
-    num_increases = 0
-    m = len(processed)
-    last = sum([processed[i] for i in range(3)])
-    for i in range(1, m):
-        cur = sum([processed[j] if j < m else 0 for j in range(i, i + 3)])
-        if cur > last:
-            num_increases += 1
-        last = cur
-    return num_increases
+
+def part2(data):
+    p2 = lambda i, m, data: sum([data[j] if j < m else 0 for j in range(i, i + 3)])
+    return get_num_increases(data, p2)
 
 if __name__ == "__main__":
-    if not len(sys.argv) > 1:
-        print("Please provide a file argument")
-    else:
-        processed = preprocess(sys.argv[1])
-        print(part1(processed))
-        print(part2(processed))
+    data = parse(sys.argv[1])
+    print(part1(data))
+    print(part2(data))
