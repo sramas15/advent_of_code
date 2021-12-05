e0c3b262be3065415267ca6b11cbc9e3802faa7e
Sheila Ramaswamy <sheilar@alumni.stanford.edu>
2021-12-04 21:41:13 -0800

    use defaultdict

--- 2021/day5.py
+++ 2021/day5.py
@@ -1,4 +1,5 @@
 import sys
+from collections import defaultdict
 
 def process_line(l):
     parts = l.split(' -> ')
@@ -11,16 +12,15 @@ def parse(fn):
         return [process_line(l.strip()) for l in f]
 
 def find_dangerous_areas(data, include_diag):
-    points = {}
+    points = defaultdict(lambda: 0)
     for coordA, coordB in data:
         if not include_diag and coordA[0] != coordB[0] and coordA[1] != coordB[1]:
             continue
         xInc = max([-1, min([1, coordB[0] - coordA[0]])])
         yInc = max([-1, min([1, coordB[1] - coordA[1]])])
-        for i in range(max([abs(coordB[0] + xInc - coordA[0]), abs(coordB[1] + yInc - coordA[1])])):
+        num_steps = abs(coordB[0] + xInc - coordA[0]) if xInc else abs(coordB[1] + yInc - coordA[1])
+        for i in range(num_steps):
             key = (coordA[0] + i * xInc, coordA[1] + i * yInc)
-            if key not in points:
-                points[key] = 0
             points[key] += 1
     return sum([1 if points[k] >= 2 else 0 for k in points])
 
