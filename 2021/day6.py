466dd1355d28ba1b0c35a42e2eaf88c32cab1747
Sheila Ramaswamy <sheilar@alumni.stanford.edu>
2021-12-06 21:43:51 -0800

    shorter day6

--- 2021/day6.py
+++ 2021/day6.py
@@ -7,20 +7,19 @@ def parse(fn):
     with open(fn) as f:
         return [process_line(l.strip()) for l in f]
 
-def num_produce(num, days_remaining, cache):
+def num_produce(num, days_remaining, cache={}):
     key = (num, days_remaining) 
     if key not in cache:
         cache[key] = 1 + sum([num_produce(8, i, cache) for i in range(days_remaining - num - 1, -1, -7)])
     return cache[key]
 
-def part1(data, cache={}):
-    return sum([num_produce(d, 80, cache) for d in data])
+def part1(data):
+    return sum([num_produce(d, 80) for d in data])
 
-def part2(data, cache={}):
-    return sum([num_produce(d, 256, cache) for d in data])
+def part2(data):
+    return sum([num_produce(d, 256) for d in data])
 
 if __name__ == "__main__":
-    cache = {}
     data = parse(sys.argv[1])
-    print(part1(data[0], cache))
-    print(part2(data[0], cache))
+    print(part1(data[0]))
+    print(part2(data[0]))
